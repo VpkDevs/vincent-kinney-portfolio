3b4ca44af15902c61f60cb701ba9c0f205430b3b
VpkDevs <183775731+VpkDevs@users.noreply.github.com>
2026-05-28 18:40:21 -0500

    Publish improved portfolio

--- _next/static/Rwf9qY1t81nuFSQiOt36v/_buildManifest.js
+++ _next/static/Rwf9qY1t81nuFSQiOt36v/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/vincent-kinney-portfolio//_next/:path+",
+        "destination": "/vincent-kinney-portfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
